d292835690b23c6bd0f6fcc126825cddd7fd1af3
Jeffrey Yasskin <jyasskin@gmail.com>
2022-10-02 14:55:49 -0700

    Get the asynchronous templates working.

--- cloud/webserver/main.py
+++ cloud/webserver/main.py
@@ -1,8 +1,12 @@
 import os
+import urllib.parse
 from asyncio import get_running_loop
-from datetime import date
+from dataclasses import dataclass
+from datetime import date, timedelta
+from typing import Optional
 
 from google.cloud import bigquery
+from google.cloud.bigquery.table import RowIterator
 from quart import Quart, stream_template
 
 client = bigquery.Client()
@@ -10,20 +14,20 @@ client = bigquery.Client()
 app = Quart(__name__)
 
 
-def wrap_future(job_or_future):
+def wrap_job(job):
     wrapped = get_running_loop().create_future()
 
     def on_ready(_):
         if wrapped.done():
             return
         try:
-            result = job_or_future.result()
+            result = job.result()
         except Exception as e:
             wrapped.set_exception(e)
             return
         wrapped.set_result(result)
 
-    job_or_future.add_done_callback(on_ready)
+    job.add_done_callback(on_ready)
     return wrapped
 
 
@@ -36,16 +40,15 @@ def without_origin(url) -> str:
 
 
 @app.template_filter()
-def web_archive(url, latest_date:str) -> str:
-    latest = date.fromisoformat(latest_date)
-    latest += timedelta(days=1)
-    return f"https://web.archive.org/web/{latest:YYYYMMDD}/{url}"
+def web_archive(url, latest_date: str) -> str:
+    end_of_latest_date = latest_date+timedelta(days=1)
+    return f"https://web.archive.org/web/{end_of_latest_date:%Y%m%d%H%M%S}/{url}"
 
 
 @app.route("/")
 async def hello_world():
     results = list(
-        await wrap_future(
+        await wrap_job(
             client.query(
                 """
                 SELECT DISTINCT crawl
@@ -58,31 +61,69 @@ async def hello_world():
     current_crawl = results[0]["crawl"]
     prev_crawl = results[1]["crawl"]
 
-    new_pages = client.query(
-        """
-        SELECT page, change
-        FROM `pbot-site-crawler.crawl.changed-pages`
-        WHERE crawl = @current_crawl
-        ORDER BY page
-        """,
-        job_config=bigquery.QueryJobConfig(
-            query_parameters=[
-                bigquery.ScalarQueryParameter("current_crawl", "DATE", current_crawl),
-            ]
-        ),
+    new_pages = get_pages_with_change(
+        current_crawl=current_crawl, change="ADD", max_results=10
+    )
+
+    removed_pages = get_pages_with_change(
+        current_crawl=current_crawl, change="DEL", max_results=10
+    )
+
+    modified_pages = get_pages_with_change(
+        current_crawl=current_crawl, change="CHANGE", max_results=10
     )
 
     return await stream_template(
         "index.html.j2",
         crawl_date=current_crawl,
         prev_crawl_date=prev_crawl,
-        new_pages=[],
-        more_new_pages=False,
-        removed_pages=[],
-        more_removed_pages=False,
-        modified_pages=[],
-        more_modified_pages=False,
+        new_pages=new_pages,
+        removed_pages=removed_pages,
+        modified_pages=modified_pages,
     )
+
+
+@dataclass
+class PagesResult:
+    pages: RowIterator
+    more_available: bool
+
+
+def get_pages_with_change(
+    *,
+    current_crawl: str,
+    change: str,
+    max_results: Optional[int] = None,
+    page_size: Optional[int] = None,
+) -> PagesResult:
+    query = client.query(
+        """
+        SELECT page
+        FROM `pbot-site-crawler.crawl.changed-pages`
+        WHERE crawl = @current_crawl AND change = @change
+        ORDER BY page
+        """,
+        job_config=bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ScalarQueryParameter("current_crawl", "DATE", current_crawl),
+                bigquery.ScalarQueryParameter("change", "STRING", change),
+            ]
+        ),
+    )
+
+    # We really only need this because Jinja doesn't await simple values, just
+    # function call results.
+    async def result():
+        await wrap_job(query)
+        results = query.result(max_results=max_results, page_size=page_size)
+        more_available = False
+        if max_results is not None and max_results < results.num_results:
+            more_available = True
+        if page_size is not None and page_size < results.num_results:
+            more_available = True
+        return PagesResult(pages=results, more_available=more_available)
+
+    return result
 
 
 if __name__ == "__main__":
