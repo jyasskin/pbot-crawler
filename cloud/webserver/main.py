7aa3df769fa7e72bc9b00faad4026fdd8f9942f9
Jeffrey Yasskin <jyasskin@gmail.com>
2022-10-02 21:33:25 -0700

    Add detail pages to list all changes of a particular type.

--- cloud/webserver/main.py
+++ cloud/webserver/main.py
@@ -3,25 +3,42 @@ import urllib.parse
 from asyncio import get_running_loop
 from dataclasses import dataclass
 from datetime import date, timedelta
-from typing import Optional
+from typing import Any, AsyncIterator, Callable, Coroutine, Optional
 
 from google.cloud import bigquery
 from google.cloud.bigquery.table import RowIterator
-from quart import Quart, stream_template
+from quart import Quart, abort, stream_template, url_for
+from werkzeug.routing import BaseConverter, ValidationError
 
 client = bigquery.Client()
 
 app = Quart(__name__)
 
 
-def wrap_job(job):
+class DateConverter(BaseConverter):
+    regex = r"\d\d\d\d-\d\d-\d\d"
+
+    def to_python(self, value: str) -> date:
+        try:
+            return date.fromisoformat(value)
+        except ValueError as e:
+            raise ValidationError() from e
+
+    def to_url(self, value: date) -> str:
+        return value.isoformat()
+
+
+app.url_map.converters["isodate"] = DateConverter
+
+
+def wrap_job(job: bigquery.QueryJob, *result_args, **result_kwargs):
     wrapped = get_running_loop().create_future()
 
     def on_ready(_):
         if wrapped.done():
             return
         try:
-            result = job.result()
+            result = job.result(*result_args, **result_kwargs)
         except Exception as e:
             wrapped.set_exception(e)
             return
@@ -39,27 +56,18 @@ def without_origin(url) -> str:
     return parsed.path
 
 
-@app.template_filter()
-def web_archive(url, latest_date: str) -> str:
-    end_of_latest_date = latest_date+timedelta(days=1)
+@app.template_filter()  # type: ignore
+def web_archive(url: str, latest_date: date) -> str:
+    end_of_latest_date = latest_date + timedelta(days=1)
     return f"https://web.archive.org/web/{end_of_latest_date:%Y%m%d%H%M%S}/{url}"
 
 
 @app.route("/")
-async def hello_world():
-    results = list(
-        await wrap_job(
-            client.query(
-                """
-                SELECT DISTINCT crawl
-                FROM `pbot-site-crawler.crawl.changed-pages`
-                ORDER BY crawl DESC
-                LIMIT 2"""
-            )
-        )
-    )
-    current_crawl = results[0]["crawl"]
-    prev_crawl = results[1]["crawl"]
+@app.route("/<isodate:crawl_date>/")
+async def root_page(crawl_date: Optional[date] = None) -> AsyncIterator[str]:
+    crawl_dates = await find_crawl_dates(crawl_date)
+    current_crawl = crawl_dates.current_crawl
+    prev_crawl = crawl_dates.prev_crawl
 
     new_pages = get_pages_with_change(
         current_crawl=current_crawl, change="ADD", max_results=10
@@ -78,24 +86,87 @@ async def hello_world():
         crawl_date=current_crawl,
         prev_crawl_date=prev_crawl,
         new_pages=new_pages,
+        more_new_pages=url_for("page_change_detail_page", crawl_date=current_crawl, change="new"),
         removed_pages=removed_pages,
+        more_removed_pages=url_for("page_change_detail_page", crawl_date=current_crawl, change="removed"),
         modified_pages=modified_pages,
+        more_modified_pages=url_for("page_change_detail_page", crawl_date=current_crawl, change="modified"),
     )
+
+
+@app.route("/<isodate:crawl_date>/<any(new,removed,modified):change>")
+async def page_change_detail_page(crawl_date: date, change: str):
+    db_change, title ,old_archive, new_archive= {
+        "new": ("ADD", f"New pages in the {crawl_date} crawl", None, "Archive"),
+        "removed": ("DEL", f"Removed pages in the {crawl_date} crawl", "Old Archive", None),
+        "modified": ("CHANGE", f"Modified pages in the {crawl_date} crawl", "Old", "New Archive"),
+    }[change]
+
+    crawl_dates: CrawlDates = await find_crawl_dates(crawl_date)
+
+    changed_pages = get_pages_with_change(
+        current_crawl=crawl_dates.current_crawl, change=db_change
+    )
+
+    return await stream_template(
+        "page_change_detail.html.j2",
+        title=title,
+        crawl_dates=crawl_dates,
+        pages=changed_pages,
+        old_archive=old_archive,
+        new_archive=new_archive,
+    )
+
+
+@dataclass(kw_only=True)
+class CrawlDates:
+    current_crawl: date
+    prev_crawl: date
+
+
+async def find_crawl_dates(current_crawl: Optional[date] = None) -> CrawlDates:
+    results = list(
+        await wrap_job(
+            client.query(
+                """
+                SELECT DISTINCT crawl
+                FROM `pbot-site-crawler.crawl.changed-pages`
+                WHERE @current_crawl is NULL OR crawl <= @current_crawl
+                ORDER BY crawl DESC
+                LIMIT 2""",
+                job_config=bigquery.QueryJobConfig(
+                    query_parameters=[
+                        bigquery.ScalarQueryParameter(
+                            "current_crawl", "DATE", current_crawl
+                        ),
+                    ]
+                ),
+            )
+        )
+    )
+    if len(results) != 2:
+        abort(404)
+    crawl_dates = CrawlDates(
+        current_crawl=results[0]["crawl"], prev_crawl=results[1]["crawl"]
+    )
+    if current_crawl is not None and crawl_dates.current_crawl != current_crawl:
+        abort(404)
+    return crawl_dates
 
 
 @dataclass
 class PagesResult:
     pages: RowIterator
     more_available: bool
+    total_rows: int
 
 
 def get_pages_with_change(
     *,
-    current_crawl: str,
+    current_crawl: date,
     change: str,
     max_results: Optional[int] = None,
-    page_size: Optional[int] = None,
-) -> PagesResult:
+) -> Callable[[], Coroutine[Any, Any, PagesResult]]:
     query = client.query(
         """
         SELECT page
@@ -114,14 +185,13 @@ def get_pages_with_change(
     # We really only need this because Jinja doesn't await simple values, just
     # function call results.
     async def result():
-        await wrap_job(query)
-        results = query.result(max_results=max_results, page_size=page_size)
+        results = await wrap_job(query, max_results=max_results)
         more_available = False
-        if max_results is not None and max_results < results.num_results:
+        if max_results is not None and max_results < results.total_rows:
             more_available = True
-        if page_size is not None and page_size < results.num_results:
-            more_available = True
-        return PagesResult(pages=results, more_available=more_available)
+        return PagesResult(
+            pages=results, more_available=more_available, total_rows=results.total_rows
+        )
 
     return result
 
