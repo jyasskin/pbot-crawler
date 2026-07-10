3bd8ff2754fbb0136bd756589991767d8ee94327
Jeffrey Yasskin <jyasskin@gmail.com>
2026-07-09 21:55:59 -0700

    Upgrade Prisma to v7.

--- fly/src/lib/prisma.ts
+++ fly/src/lib/prisma.ts
@@ -1,3 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@generated/prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import process from "node:process";
 
-export const prisma = new PrismaClient();
+const adapter = new PrismaBetterSqlite3(
+    { url: process.env.DATABASE_URL },
+    { timestampFormat: "unixepoch-ms" },
+);
+export const prisma = new PrismaClient({ adapter });
