d43262ba636d90b8f3edfd0e9d018c17f37686f3
Jeffrey Yasskin <jyasskin@gmail.com>
2026-07-20 12:00:00 -0700

    Generate Prisma code without environment variables.

--- fly/prisma.config.ts
+++ fly/prisma.config.ts
@@ -1,11 +1,10 @@
 import type { PrismaConfig } from "prisma";
-import { env } from "prisma/config";
 export default {
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env.DATABASE_URL,
   },
 } satisfies PrismaConfig;
